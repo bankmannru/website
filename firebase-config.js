f72f85c42cd5d5cbb23bfa58ee33e486e7833fc1
Ahmed <utoplennik69pc@gmail.com>
2025-02-06 20:05:54 +0300

    sdfds

--- firebase-config.js
+++ firebase-config.js
@@ -20,6 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getDatabase();
-export const auth = getAuth();
-export { app };
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+export { database, auth, app };
