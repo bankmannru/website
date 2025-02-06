386cbca626fc82bf240506b31e77a1075d26d610
Ahmed <utoplennik69pc@gmail.com>
2025-02-06 18:49:30 +0300

    GAMES

--- firebase-config.js
+++ firebase-config.js
@@ -8,18 +8,17 @@ import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 
 const firebaseConfig = {
-  apiKey: "your-api-key",
+  apiKey: "AIzaSyAK8cgAKNDAfXIOzA_0kn4QhVk5OYkcGbk",
   authDomain: "bank-mannru2.firebaseapp.com",
   databaseURL: "https://bank-mannru2-default-rtdb.firebaseio.com",
   projectId: "bank-mannru2",
-  storageBucket: "bank-mannru2.appspot.com",
+  storageBucket: "bank-mannru2.firebasestorage.app",
   messagingSenderId: "666672164834",
-  appId: "1:666672164834:web:d221a89a4c4ed88d4c13bb"
+  appId: "1:666672164834:web:d221a89a4c4ed88d4c13bb",
+  measurementId: "G-85BL0L97NP"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const auth = getAuth(app);
-
-export { database, auth }; 
+export const database = getDatabase(app);
+export const auth = getAuth(app); 
